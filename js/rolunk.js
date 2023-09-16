8e57464a01580bb04d90a680dd652430419b7af8
PDani <pinteadani88@gmail.com>
2023-09-16 10:24:09 +0200

    Bemutatkozások elkezdve

--- js/rolunk.js
+++ js/rolunk.js
@@ -1,9 +1,31 @@
 var Ora;
+var Bemutatkozasok = [{
+    nev: "Kósik János",
+    img: "img src",
+    szoveg: ""},{
+    nev: "Orosz ádám",
+    img: "img src",
+    szoveg: ""},{
+    nev: "Pintea Dániel",
+    img: "img src",
+    szoveg: ""}
+];
+
+//ora
 function TimeLoop(){
-    const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    if(minutes.toString().length == 1){
+        minutes = '0'+minutes;
+    }
     document.getElementById('ora').innerHTML = "<p>"+hours+":"+minutes+"</p>";
+}
+//---
+
+function Bemutatkozas(num){
+    document.getElementById('Bemutatkozas').classList.add("Megjelen");
+    document.getElementById('BlackBackground').classList.add('BackMegjelen')
 }
 
 function Main(){
