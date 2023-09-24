59dddc8522c0bd24b0af73159fe7ef64fdcc508c
PDani <pinteadani88@gmail.com>
2023-09-24 16:25:30 +0200

    Body elkezdve

--- js/filmek.js
+++ js/filmek.js
@@ -1,5 +1,17 @@
 function SearchBarShow(){
     if(document.getElementsByClassName('SearchShow').length == 0){
         document.getElementById("SearchInput").classList += " SearchShow";
+        setTimeout(SearchTime,7000);
     }
+}
+
+function SearchTime(){
+    if(document.getElementById('SearchInput').value == ""){
+        document.getElementById("SearchInput").classList.add("SearchHide");
+        setTimeout(SearchClassRemove,500);
+    }
+}
+
+function SearchClassRemove(){
+    document.getElementById("SearchInput").classList = "SearchInput";
 }
